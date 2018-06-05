30b625b18addfa42ce84d59702b221e3fdae348b
Rachit Solanki <rachitpsolanki@gmail.com>
2018-06-05 16:03:13 +0530

    added Gallery & Header

---
+++ src/components/Gallery.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="gallery">
+        <div class="title">
+            <h2>Image Gallery</h2>
+            <p>Some description about this awesome image gallery made using vue.js</p>
+        <div>
+        </div>
+            <img class="image-item" src="../assets/logo.png" height="200" width="200"  v-for="i in 10" :key='i'/>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'Gallery'
+}
+</script>
+<style>
+.gallery .image-item{
+margin: 5px;
+}
+.gallery .title *{
+    text-align: center;
+}
+.gallery img{
+    transition: ease-in-out;
+    transition-duration: 0.2s;
+}
+.gallery img:hover{
+    transform: scale(1.2);
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="header">
+        <div class="item">
+            <p>Menu 1</p> 
+            <span class="menu1">
+                <ul>Some menu item 1</ul>
+                <ul>Some menu item 1</ul>
+                <ul>Some menu item 1</ul>
+                <ul>Some menu item 1</ul>
+            </span>        
+        </div>
+        <div class="item">
+            <p>Menu 2</p>
+            <span class="menu2">
+                <ul>Some menu item 2</ul>
+                <ul>Some menu item 2</ul>
+                <ul>Some menu item 2</ul>
+                <ul>Some menu item 2</ul>
+            </span>        
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'Header'
+}
+</script>
+<style >
+.header{
+    background: #efefef;
+    width: 100%;
+    border-radius: 10px;
+    height: 56px;
+    position: relative;
+    z-index: 1;
+}
+.header .item{
+    display: inline-block;
+    height: 56px;
+    padding: 0px 5px;
+}
+.item p{
+    line-height: 56px;
+    background:transparent;
+    margin: 0px;
+
+}
+ p:hover + span{
+    display: block;
+}
+
+.item span{
+    display: none;
+    background: #e3e3e3;
+    position: absolute;
+    margin-top:10px; 
+    width: 100%;
+    left: 0;
+    border-radius: 10px;
+}
+ul{
+    padding: 0px;
+    margin-left: 10px;
+}
+
+</style>
